c488647ac714770cb181a789923b128f2b2f8f99
joshuadargan <joshuadargan282@ufl.edu>
2020-10-24 18:52:46 -0400

    Return Cart Updates

---
+++ Overdue/Assets/Scripts/ReturnCartManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ReturnCartManager : MonoBehaviour
+{
+    private static List<GameObject> ReturnCarts;
+    public const int NumCarts = 7;
+
+    public static void AddReturnCart(GameObject rc)
+    {
+        ReturnCarts.Add(rc);
+        //Randomly select a cart to become the active cart
+        if (ReturnCarts.Count >= NumCarts)
+        {
+            System.Random rnd = new System.Random();
+            int activeIndex = rnd.Next(0, ReturnCarts.Count);
+            for (int i = 0; i < ReturnCarts.Count; i++)
+            {
+                if (activeIndex == i)
+                {
+                    ReturnCarts[i].SetActive(true);
+                    GameObjectiveUIText.SetObjectiveText("Current Objective: " + ReturnCarts[i].name);
+                }
+                else
+                {
+                    ReturnCarts[i].SetActive(false);
+                }
+            }
+        }
+    }
+}
