dbc786414361bf2a42657ca7d126b0a968ee04d2
joshuadargan <joshuadargan282@ufl.edu>
2020-10-25 20:56:46 -0400

    test ai and navmeshplus

---
+++ Overdue/Assets/Scripts/LibrarianBehavior.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class LibrarianBehavior : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        var agent = GetComponent<NavMeshAgent>();
+        agent.updateRotation = false;
+        agent.updateUpAxis = false;
+    }
+
+    //// Update is called once per frame
+    //void Update()
+    //{
+        
+    //}
+}
