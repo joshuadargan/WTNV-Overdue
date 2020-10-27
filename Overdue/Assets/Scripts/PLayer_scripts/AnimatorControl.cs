f7babd1a55694944a548618430df086b4e055ffa
Nightcoo <cristianvillaran@ufl.edu>
2020-10-26 20:48:54 -0400

    animator changes and wall collider fix

---
+++ Overdue/Assets/Scripts/PLayer_scripts/AnimatorControl.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Security.Cryptography;
+using UnityEngine;
+
+public class AnimatorControl : MonoBehaviour
+{
+    public Animator animator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        animator.SetFloat("Horizontal", Input.GetAxis("Horizontal"));
+        animator.SetFloat("Vertical", Input.GetAxis("Vertical"));
+    }
+}
