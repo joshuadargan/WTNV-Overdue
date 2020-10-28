6ed4660e8a8919f47933c956f639bbdd84036c72
Cristian Villaran <Cristianvillaran@ufl.edu>
2020-10-28 11:58:34 -0400

    ANIMATOR FIX

--- Overdue/Assets/Scripts/PLayer_scripts/AnimatorControl.cs
+++ Overdue/Assets/Scripts/PLayer_scripts/AnimatorControl.cs
@@ -17,7 +17,31 @@ public class AnimatorControl : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        animator.SetFloat("Horizontal", Input.GetAxis("Horizontal"));
-        animator.SetFloat("Vertical", Input.GetAxis("Vertical"));
+        //animator.SetFloat("Horizontal", Input.GetAxis("Horizontal"));
+        //animator.SetFloat("Vertical", Input.GetAxis("Vertical"));
+        
+        Vector3 mousePos = Input.mousePosition;
+        mousePos.z = 0;
+        
+        Vector3 objectPos = Camera.main.WorldToScreenPoint(transform.position);
+        mousePos.x = mousePos.x - objectPos.x;
+        mousePos.y = mousePos.y - objectPos.y;
+        
+        float angle = Mathf.Atan2(mousePos.y, mousePos.x) * Mathf.Rad2Deg;
+        
+        //Debug.Log("Angle = " + angle);
+        
+        animator.SetFloat("angle", angle);
+ 
+        
+       /* if (angle > -45 && angle < 45){
+        	
+        }else if(angle > 45 && angle < 135){
+        
+        }else if(angle > 135 || angle < -135){
+        
+        }else if(angle > -135 && angle < -45){
+        
+        }*/
     }
 }
