ccec492518af15dbd5ddebd3d61d3b00d91111f3
Cristian Villaran <Cristianvillaran@ufl.edu>
2020-11-07 21:11:22 -0500

    table sprite swap progress

---
+++ Overdue/Assets/Scripts/Environment_Scripts/Table_Sprite_Swtich.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Table_Sprite_Swtich : MonoBehaviour
+{
+	
+	public GameObject Object;
+	
+	public const string crouch_layer = "Crouching";
+	public Spriterender player;
+	
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    
+    void OnTriggerEnter2D(Collider2D player){
+    	 if(player.tag == "Player" && player.sortingLayerName == crouch_layer){
+         	Object.SetActive(true);
+         	
+         	
+     	}
+    	
+    }
+    
+    void OnTriggerExit2D(Collider2D player){
+    	 if(player.tag == "Player"){
+         	Object.SetActive(false);
+     	}
+    	
+    }
+}
