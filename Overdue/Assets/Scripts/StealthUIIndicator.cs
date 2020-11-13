ba1e821b9dc0ff8d7d53277948f5af5f54af487d
joshuadargan <joshuadargan282@ufl.edu>
2020-11-12 20:59:45 -0500

    stealth eye progress

--- Overdue/Assets/Scripts/StealthUIIndicator.cs
+++ Overdue/Assets/Scripts/StealthUIIndicator.cs
@@ -11,20 +11,64 @@ public class StealthUIIndicator : MonoBehaviour
         Ajar,
         Open
     }
-    private static Image UIEye;
+
+    private const string Closed = "Closed";
+    private const string Ajar = "Ajar";
+    private const string Open = "Open";
+
+    private static Image[] UIEyes;
     public static EyeState CurrentEyeState { get; private set; }
 
     // Start is called before the first frame update
     void Start()
     {
-        UIEye = GetComponent<Image>();
+        UIEyes = GetComponentsInChildren<Image>();
+
     }
 
     public static void SetUIEyeState(EyeState state)
     {
-        if (state != CurrentEyeState)
+        switch(state)
         {
-            CurrentEyeState = state;
+            case EyeState.Closed:
+                foreach (Image eye in UIEyes)
+                {
+                    if (eye.name.CompareTo(Closed) == 0)
+                    {
+                        eye.enabled = true;
+                    }
+                    else
+                    {
+                        eye.enabled = false;
+                    }
+                }
+                break;
+            case EyeState.Ajar:
+                foreach (Image eye in UIEyes)
+                {
+                    if (eye.name.CompareTo(Ajar) == 0)
+                    {
+                        eye.enabled = true;
+                    }
+                    else
+                    {
+                        eye.enabled = false;
+                    }
+                }
+                break;
+            case EyeState.Open:
+                foreach (Image eye in UIEyes)
+                {
+                    if (eye.name.CompareTo(Open) == 0)
+                    {
+                        eye.enabled = true;
+                    }
+                    else
+                    {
+                        eye.enabled = false;
+                    }
+                }
+                break;
         }
     }
     
