9abd2e14a38703a1800a39522b77870d6b169e7a
joshuadargan <joshuadargan282@ufl.edu>
2020-11-12 20:28:37 -0500

    Fixed pause menu

---
+++ Overdue/Assets/Scripts/StealthUIIndicator.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class StealthUIIndicator : MonoBehaviour
+{
+    public enum EyeState
+    {
+        Closed,
+        Ajar,
+        Open
+    }
+    private static Image UIEye;
+    public static EyeState CurrentEyeState { get; private set; }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        UIEye = GetComponent<Image>();
+    }
+
+    public static void SetUIEyeState(EyeState state)
+    {
+        if (state != CurrentEyeState)
+        {
+            CurrentEyeState = state;
+        }
+    }
+    
+}
