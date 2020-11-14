1c525cc97e55fc830f68d24894c965e9418915ab
joshuadargan <joshuadargan282@ufl.edu>
2020-11-14 17:44:07 -0500

    test

--- Overdue/Assets/Scripts/UI Scripts/ReadCollectible.cs
+++ Overdue/Assets/Scripts/UI Scripts/ReadCollectible.cs
@@ -8,16 +8,35 @@ public class ReadCollectible : MonoBehaviour
     [SerializeField] public GameObject collectibleMenuGroup;
     [SerializeField] private bool isPaused;
 
+    public static string lore;
+
     void Start()
     {
         collectibleMenuGroup.SetActive(false);
+        lore = "";
     }
 
     void Update()
     {
+        if (lore.Length == 0)
+        {
+            if (isPaused)
+            {
+                Close();
+            }
+        }
+        else if (!isPaused)
+        {
+            Read();
+        }
     }
 
-    public void Pause()
+    public static void SetLore(string l)
+    {
+        lore = l;
+    }
+
+    public void Read()
     {
         Time.timeScale = 0f;
         collectibleMenuGroup.SetActive(true);
@@ -25,11 +44,12 @@ public class ReadCollectible : MonoBehaviour
         PauseGame.IsPaused = true;
     }
 
-    public void Unpause()
+    public void Close()
     {
         Time.timeScale = 1f;
         collectibleMenuGroup.SetActive(false);
         isPaused = false;
         PauseGame.IsPaused = false;
+
     }
 }
