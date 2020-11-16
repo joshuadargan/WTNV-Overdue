2ed7dc973bb2a7b177c43ff330ef2203c231fcf6
Cristian Villaran <Cristianvillaran@ufl.edu>
2020-11-15 22:48:41 -0500

    Revert "Librarian animator and canvas adjustment"
    This reverts commit 4bcb4bf0e441dfa1d71c6723969e61193c3936c4.

--- Overdue/Assets/Scripts/LibrarianBehavior.cs
+++ Overdue/Assets/Scripts/LibrarianBehavior.cs
@@ -99,11 +99,10 @@ public class LibrarianBehavior : MonoBehaviour
        animator.SetFloat("Horizontal", Horizontal);
        animator.SetFloat("Vertical", Vertical);
        animator.SetBool("VoverH", VoverH);
-       animator.SetFloat("Suspicion", suspicion);
 
        prevPos = transform.position;
 
-	
+
     }
 
     void OnCollisionEnter2D(Collision2D collision)
