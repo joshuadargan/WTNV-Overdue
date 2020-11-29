7084577818579c4e5f6ff24f6a25c96e1b028122
Nightcoo <cristianvillaran@ufl.edu>
2020-11-29 01:51:55 -0500

    Tutorial work

---
+++ Overdue/Assets/Scripts/Tutorial_Dialogue_Trigger.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Tutorial_Dialogue_Trigger : MonoBehaviour
+{
+    private string text;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public virtual void Read()
+    {
+        GameObject.Find(GameObjectNames.Canvas).GetComponent<PauseGame>().Read();
+        GameObject.Find(GameObjectNames.PaperText).GetComponent<Text>().text = text;
+    }
+}
