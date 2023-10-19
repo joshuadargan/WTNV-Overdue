2d30b6e68f6c5e9397d142a5270ed83652c7619e
Nightcoo <cristianvillaran@ufl.edu>
2023-10-18 21:24:41 -0400

    fixed librarians phasing through objects when they are not supposed too
    they're tool tall. I had to use layered sprite masks

---
+++ Overdue/Assets/Scripts/LibrarianSpriteMatch.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LibrarianSpriteMatch : MonoBehaviour
+{
+    public SpriteRenderer baseLayer;
+    SpriteRenderer maskLayer;
+    void Start()
+    {
+        maskLayer = gameObject.GetComponent<SpriteRenderer>();   
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (maskLayer.sprite != baseLayer.sprite) {
+
+            maskLayer.sprite = baseLayer.sprite;    
+        }
+    }
+}
