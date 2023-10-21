5f51a84c1661d6b829ba0de456239ab737fda0e3
Nightcoo <cristianvillaran@ufl.edu>
2023-10-21 19:15:36 -0400

    script added where half-height barriers create shadows when the Player is crouching

---
+++ Overdue/Assets/Scripts/Environment_Scripts/ToggleLightBlock.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ToggleLightBlock : MonoBehaviour
+{
+    public GameObject Shadow;
+    private bool isCrouching;
+
+    void Update()
+    {
+        if (Input.GetButton("Crouch"))
+        {
+            Shadow.SetActive(true);
+           // gameObject.GetComponent<PolygonCollider2D>().enabled = false;
+            gameObject.layer = 0;                                            //set layer to block raycasts
+
+            isCrouching = true;
+        }
+        else if (isCrouching == true) {
+
+            Shadow.SetActive(false);
+           // gameObject.GetComponent<PolygonCollider2D>().enabled = true;
+            gameObject.layer = 2;                                           //set layer it ignore raycasts
+
+            isCrouching = false;
+        }
+    }
+}
